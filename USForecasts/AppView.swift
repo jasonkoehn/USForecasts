f8f0f9e94b15c05e55bc0a5158aadb6b36610db2
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-10-10 11:52:05 -0700

    Initial Commit

---
+++ USForecasts/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  USForecasts
+//
+//  Created by Jason Koehn on 10/10/23.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppView()
+}
